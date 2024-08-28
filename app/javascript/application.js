f4e2373c42a825401ba85eb02cdb41b23bc3b085
Malin Mabika <malin@newschool.edu>
2024-08-29 00:15:16 +0200

    Removing all bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+
 import "./liveDateAndTime"
+
 
 // require("@rails/ujs").start()

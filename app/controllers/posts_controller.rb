dd7faef6c31e05cf6ae327c432a295008ecd23ce
Malin Mabika <malin@newschool.edu>
2024-08-29 13:54:13 +0200

    Adding new post links to user_posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :user_posts]
   before_action :set_post, only: %i[show edit update destroy]
 
   def index

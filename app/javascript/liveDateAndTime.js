3ff3cb15ce8199e860f390e0e3efad7f598db28d
Malin Mabika <malin@newschool.edu>
2024-08-29 13:58:48 +0200

    Adding a DOMLOAD event listener to live date and time

--- app/javascript/liveDateAndTime.js
+++ app/javascript/liveDateAndTime.js
@@ -1,15 +1,19 @@
-console.log("Live Date and Time JS Connected");
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("DOMContentLoaded")
+  console.log("Live Date and Time JS Connected");
 
-function display_c() {
-  const refresh = 1000; // Refresh rate in milliseconds
-  setTimeout(display_ct, refresh);
-}
+  function display_c() {
+    const refresh = 1000; // Refresh rate in milliseconds
+    setTimeout(display_ct, refresh);
+  }
 
-function display_ct() {
-  const x = new Date();
-  document.getElementById('ct').innerHTML = x;
-  display_c();
-}
+  function display_ct() {
+    const x = new Date();
+    document.getElementById('ct').innerHTML = x;
+    display_c();
+  }
 
-// Start the clock
-display_ct();
+  // Start the clock
+  display_ct();
+
+});
